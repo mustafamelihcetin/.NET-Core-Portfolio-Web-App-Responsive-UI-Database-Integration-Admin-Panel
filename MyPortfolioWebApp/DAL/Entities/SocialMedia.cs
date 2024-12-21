8a8802775177cfdca6dd11844af4f063989315f1
Melih <mustafamelihcetin@outlook.com>
2024-12-22 02:51:46 +0300

    Contact, message and social media entities added.

---
+++ MyPortfolioWebApp/DAL/Entities/SocialMedia.cs
@@ -0,0 +1,10 @@
+﻿namespace MyPortfolioWebApp.DAL.Entities
+{
+    public class SocialMedia
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+    }
+}
