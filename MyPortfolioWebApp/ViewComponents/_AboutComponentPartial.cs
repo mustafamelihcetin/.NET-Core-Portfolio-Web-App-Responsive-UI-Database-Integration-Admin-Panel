6849fe5bc36d1f9bacd1e2aa37f381e068ee10e7
Melih <mustafamelihcetin@outlook.com>
2024-12-25 23:58:52 +0300

    AboutComponentPartial

---
+++ MyPortfolioWebApp/ViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioWebApp.ViewComponents
+{
+    public class _AboutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
