ba2884f1f57e3d66ccbf3cacac4e33cd2add8cce
Melih <mustafamelihcetin@outlook.com>
2024-12-27 23:45:45 +0300

    Bug fixes in about section.

---
+++ MyPortfolioWebApp/ViewComponents/_SkillComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioWebApp.ViewComponents
+{
+    public class _SkillComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
