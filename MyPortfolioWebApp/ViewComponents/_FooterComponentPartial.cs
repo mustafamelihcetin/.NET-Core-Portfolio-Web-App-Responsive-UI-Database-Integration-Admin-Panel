5df672c3cfc8a1d6e5625f276e2aeec4f0b4a0a2
Melih <mustafamelihcetin@outlook.com>
2025-01-02 02:19:25 +0300

    Footer Partial

---
+++ MyPortfolioWebApp/ViewComponents/_FooterComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioWebApp.ViewComponents
+{
+    public class _FooterComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() { return View(); }
+    }
+}
