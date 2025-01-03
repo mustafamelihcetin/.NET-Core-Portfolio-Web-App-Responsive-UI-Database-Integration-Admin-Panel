75e150736969dbd96b5a1a34ae61619a84091ff8
Melih <mustafamelihcetin@outlook.com>
2025-01-04 01:34:34 +0300

    ExperienceController for Admin Panel.

--- MyPortfolioWebApp/Controllers/ExperienceController.cs
+++ MyPortfolioWebApp/Controllers/ExperienceController.cs
@@ -8,77 +8,9 @@ namespace MyPortfolioWebApp.Controllers
 {
     public class ExperienceController : Controller
     {
-        private readonly MyPortfolioContext _context;
-
-        public ExperienceController(MyPortfolioContext context)
-        {
-            _context = context;
-        }
-        public IActionResult Index()
-        {
-            // Veritabanından tüm deneyimleri liste olarak al
-            var model = _context.Experiences?.ToList() ?? new List<Experience>();
-            return View(model);
-        }
-
         public IActionResult ExperienceList()
         {
-            var values = _context.Experiences?.ToList() ?? new List<Experience>();
-            return View(values);
-        }
-
-        [HttpGet]
-        public IActionResult CreateExperience()
-        {
             return View();
-        }
-
-        [HttpPost]
-        public IActionResult CreateExperience(Experience experience)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Experiences.Add(experience);
-                _context.SaveChanges();
-                return RedirectToAction("ExperienceList");
-            }
-            return View(experience);
-        }
-
-        public IActionResult DeleteExperience(int id)
-        {
-            var value = _context.Experiences.Find(id);
-            if (value == null)
-            {
-                return NotFound();
-            }
-
-            _context.Experiences.Remove(value);
-            _context.SaveChanges();
-            return RedirectToAction("ExperienceList");
-        }
-
-        [HttpGet]
-        public IActionResult UpdateExperience(int id)
-        {
-            var value = _context.Experiences.Find(id);
-            if (value == null)
-            {
-                return NotFound();
-            }
-            return View(value);
-        }
-
-        [HttpPost]
-        public IActionResult UpdateExperience(Experience experience)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Experiences.Update(experience);
-                _context.SaveChanges();
-                return RedirectToAction("ExperienceList");
-            }
-            return View(experience);
         }
     }
 }
