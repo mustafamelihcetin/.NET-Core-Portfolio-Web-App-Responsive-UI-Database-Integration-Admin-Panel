79e36c81825857fa85791615a2f63dedddeaeda0
Melih <mustafamelihcetin@outlook.com>
2025-01-16 22:43:01 +0300

    Sidebar Layout Component.

---
+++ MyPortfolioWebApp/ViewComponents/LayoutViewComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioWebApp.ViewComponents.LayoutViewComponents
+{
+	public class _LayoutSidebarComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke() {  return View(); }
+	}
+}
