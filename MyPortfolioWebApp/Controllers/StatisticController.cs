da5ff94aaa2229504444d1ab749899b0642e94a5
Melih <mustafamelihcetin@outlook.com>
2025-01-26 22:32:34 +0300

    Statistic controller.

--- MyPortfolioWebApp/Controllers/StatisticController.cs
+++ MyPortfolioWebApp/Controllers/StatisticController.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioWebApp.DAL.Context;
 
 namespace MyPortfolioWebApp.Controllers
 {
     public class StatisticController : Controller
-    {
+    {        
+        MyPortfolioContext context = new MyPortfolioContext();
         public IActionResult Index()
         {
+            ViewBag.v1 = context.Skills.Count();
+            ViewBag.v2 = context.Skills.Count();
             return View();
         }
     }
