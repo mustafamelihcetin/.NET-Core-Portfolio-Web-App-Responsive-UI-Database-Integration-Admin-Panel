6f384ce3338d9747484f9569dcec7bbe9c4e3a3b
Melih <mustafamelihcetin@outlook.com>
2025-01-27 23:54:35 +0300

    MessageController.

---
+++ MyPortfolioWebApp/Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyPortfolioWebApp.Controllers
+{
+	public class MessageController : Controller
+	{
+		public IActionResult Inbox()
+		{
+			return View();
+		}
+	}
+}
