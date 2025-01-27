b4945a020c712e17320411ebdcb6fa7c0f59c6d9
Melih <mustafamelihcetin@outlook.com>
2025-01-27 23:50:18 +0300

    Creating statistics dynamically.

--- MyPortfolioWebApp/Controllers/StatisticController.cs
+++ MyPortfolioWebApp/Controllers/StatisticController.cs
@@ -9,7 +9,9 @@ namespace MyPortfolioWebApp.Controllers
         public IActionResult Index()
         {
             ViewBag.v1 = context.Skills.Count();
-            ViewBag.v2 = context.Skills.Count();
+            ViewBag.v2 = context.Messages.Count();
+            ViewBag.v3 = context.Messages.Where(x => x.IsRead == false).Count();
+            ViewBag.v4 = context.Messages.Where(x => x.IsRead == true).Count();
             return View();
         }
     }
