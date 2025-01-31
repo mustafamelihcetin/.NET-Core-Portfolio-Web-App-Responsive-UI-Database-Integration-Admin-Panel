c16c908ac7cab514a3f6441e4dbbca5d33cebe5f
Melih <mustafamelihcetin@outlook.com>
2025-01-31 22:40:42 +0300

    IsRead conditions.

--- MyPortfolioWebApp/Controllers/MessageController.cs
+++ MyPortfolioWebApp/Controllers/MessageController.cs
@@ -3,13 +3,27 @@ using MyPortfolioWebApp.DAL.Context;
 
 namespace MyPortfolioWebApp.Controllers
 {
-	public class MessageController : Controller
-	{
-		MyPortfolioContext context= new MyPortfolioContext();
-		public IActionResult Inbox()
-		{
-			var values = context.Messages.ToList();
-			return View(values);
-		}
-	}
+    public class MessageController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult Inbox()
+        {
+            var values = context.Messages.ToList();
+            return View(values);
+        }
+        public IActionResult ChangeIsReadToTrue(int id)
+        {
+            var value = context.Messages.Find(id);
+            value.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public IActionResult ChangeIsReadToFalse(int id)
+        {
+            var value = context.Messages.Find(id);
+            value.IsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+    }
 }
