ad43ede628cfb0a318efc7f34113ff00ff8ccf99
Melih <mustafamelihcetin@outlook.com>
2025-01-31 21:57:50 +0300

    Message/Inbox.

--- MyPortfolioWebApp/Controllers/MessageController.cs
+++ MyPortfolioWebApp/Controllers/MessageController.cs
@@ -1,12 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioWebApp.DAL.Context;
 
 namespace MyPortfolioWebApp.Controllers
 {
 	public class MessageController : Controller
 	{
+		MyPortfolioContext context= new MyPortfolioContext();
 		public IActionResult Inbox()
 		{
-			return View();
+			var values = context.Messages.ToList();
+			return View(values);
 		}
 	}
 }
