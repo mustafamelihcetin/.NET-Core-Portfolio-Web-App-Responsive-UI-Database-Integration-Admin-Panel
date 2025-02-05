9879e8673868f2c2e4f4dc254448c6c9692958ee
Melih <mustafamelihcetin@outlook.com>
2025-02-05 23:58:43 +0300

    ToDoList Entity.

--- MyPortfolioWebApp/DAL/Context/MediumService.cs
+++ MyPortfolioWebApp/DAL/Context/MediumService.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.ServiceModel.Syndication;
-using System.Xml;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.ServiceModel.Syndication;
+//using System.Xml;
 
-public class MediumService
-{
-    private const string MediumRssUrl = "https://medium.com/feed/@melihh";
+//public class MediumService
+//{
+//    private const string MediumRssUrl = "https://medium.com/feed/@melihh";
 
-    public List<BlogPost> GetMediumPosts()
-    {
-        var blogPosts = new List<BlogPost>();
+//    public List<BlogPost> GetMediumPosts()
+//    {
+//        var blogPosts = new List<BlogPost>();
 
-        using (var reader = XmlReader.Create(MediumRssUrl))
-        {
-            var feed = SyndicationFeed.Load(reader);
-            if (feed != null)
-            {
-                blogPosts = feed.Items.Select(item => new BlogPost
-                {
-                    Title = item.Title.Text,
-                    PublishedDate = item.PublishDate.DateTime,
-                    Link = item.Links.FirstOrDefault()?.Uri.ToString(),
-                    Summary = item.Summary?.Text
-                }).ToList();
-            }
-        }
+//        using (var reader = XmlReader.Create(MediumRssUrl))
+//        {
+//            var feed = SyndicationFeed.Load(reader);
+//            if (feed != null)
+//            {
+//                blogPosts = feed.Items.Select(item => new BlogPost
+//                {
+//                    Title = item.Title.Text,
+//                    PublishedDate = item.PublishDate.DateTime,
+//                    Link = item.Links.FirstOrDefault()?.Uri.ToString(),
+//                    Summary = item.Summary?.Text
+//                }).ToList();
+//            }
+//        }
 
-        return blogPosts;
-    }
-}
+//        return blogPosts;
+//    }
+//}
 
-public class BlogPost
-{
-    public string Title { get; set; }
-    public DateTime PublishedDate { get; set; }
-    public string Link { get; set; }
-    public string Summary { get; set; }
-}
+//public class BlogPost
+//{
+//    public string Title { get; set; }
+//    public DateTime PublishedDate { get; set; }
+//    public string Link { get; set; }
+//    public string Summary { get; set; }
+//}
---
+++ MyPortfolioWebApp/DAL/Entities/ToDoList.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPortfolioWebApp.DAL.Entities
+{
+    public class ToDoList
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string ImgUrl { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+    }
+}
