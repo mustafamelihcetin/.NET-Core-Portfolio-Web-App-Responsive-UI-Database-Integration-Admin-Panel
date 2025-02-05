9a9fd1f4eb81108b854d64b66dae0eb5ebabba8d
Melih <mustafamelihcetin@outlook.com>
2025-02-06 00:18:48 +0300

    ToDoList Controller.

---
+++ MyPortfolioWebApp/Controllers/ToDoListController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioWebApp.DAL.Context;
+using MyPortfolioWebApp.DAL.Entities;
+
+namespace MyPortfolioWebApp.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult Index()
+        {
+            var values = context.ToDoLists.ToList();
+            return View(context);
+        }
+        [HttpGet]
+        public IActionResult CreateToDoList()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateToDoList(ToDoList toDoList)
+        {
+            context.ToDoLists.Add(toDoList);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteToDoList(int id)
+        {
+            var value = context.ToDoLists.Find(id);
+            context.ToDoLists.Remove(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateToDoList(int id)
+        {
+            var value = context.ToDoLists.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public IActionResult UpdateToDoList(ToDoList toDoList)
+        {
+            context.ToDoLists.Update(toDoList);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MyPortfolioWebApp/Migrations/20250205210609_mig2.Designer.cs
+++ MyPortfolioWebApp/Migrations/20250205210609_mig2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyPortfolioWebApp.DAL.Context;
 
@@ -11,9 +12,11 @@ using MyPortfolioWebApp.DAL.Context;
 namespace MyPortfolioWebApp.Migrations
 {
     [DbContext(typeof(MyPortfolioContext))]
-    partial class MyPortfolioContextModelSnapshot : ModelSnapshot
+    [Migration("20250205210609_mig2")]
+    partial class mig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
