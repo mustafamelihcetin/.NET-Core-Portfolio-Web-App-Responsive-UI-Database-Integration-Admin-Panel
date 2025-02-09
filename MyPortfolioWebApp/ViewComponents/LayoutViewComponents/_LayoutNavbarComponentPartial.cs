3c5af801f9f8c96f70824c080a1a9541358d9660
Melih <mustafamelihcetin@outlook.com>
2025-02-09 23:44:12 +0300

    Notifications.

--- MyPortfolioWebApp/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
+++ MyPortfolioWebApp/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolioWebApp.DAL.Context;
 
 namespace MyPortfolioWebApp.ViewComponents.LayoutViewComponents
 {
-	public class _LayoutNavbarComponentPartial:ViewComponent
-	{
-		public IViewComponentResult Invoke() {  return View(); }
-	}
+    public class _LayoutNavbarComponentPartial : ViewComponent
+    {
+        MyPortfolioContext _context = new MyPortfolioContext();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.toDoListCount = _context.ToDoLists.Where(x => x.Status == false).Count();
+            var values = _context.ToDoLists.Where(x => x.Status == false).ToList();
+            return View(values);
+        }
+    }
 }
